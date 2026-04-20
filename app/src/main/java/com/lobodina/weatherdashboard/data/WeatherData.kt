4ab222f0861c5a96b8c36c69c82cf070d52c840a
grehochka82 <lobodina0607@mail.ru>
2026-04-20 23:39:16 +0300

    Step 4: Create WeatherRepository with suspend functions

---
+++ app/src/main/java/com/lobodina/weatherdashboard/data/WeatherData.kt
@@ -0,0 +1,11 @@
+package com.lobodina.weatherdashboard.data
+
+data class WeatherData(
+    val temperature: Int? = null,
+    val humidity: Int? = null,
+    val windSpeed: Int? = null,
+    val weatherIndex: Int? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val loadingProgress: String = ""
+)
