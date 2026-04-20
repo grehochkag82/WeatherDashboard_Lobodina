efca6927aa30e6e26b78950f77f6412eb53b9ea4
grehochka82 <lobodina0607@mail.ru>
2026-04-20 23:34:16 +0300

    Step 2: Add coroutines dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
     }
 }
 
-dependencies {
+dependencies  {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
