42b1be77d76d6529830ef6e3b3f861a2d8a0c7c5
grehochka82 <lobodina0607@mail.ru>
2026-04-21 05:59:30 +0300

    Step 5: Create ViewModel with sequential loading

---
+++ app/src/main/java/com/lobodina/weatherdashboard/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,54 @@
+package com.lobodina.weatherdashboard.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lobodina.weatherdashboard.data.WeatherData
+import com.lobodina.weatherdashboard.data.WeatherRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class WeatherViewModel : ViewModel() {
+
+    private val repository = WeatherRepository()
+    private val _weatherState = MutableStateFlow(WeatherData())
+    val weatherState: StateFlow<WeatherData> = _weatherState.asStateFlow()
+
+    init {
+        loadWeatherData()
+
+    }
+
+    fun loadWeatherData() {
+        viewModelScope.launch {
+            _weatherState.value = _weatherState.value.copy(
+                isLoading = true,
+                error = null
+            )
+            try {
+                val temperature = repository.fetchTemperature()  // 2 сек
+                _weatherState.value = _weatherState.value.copy(temperature = temperature)
+                val humidity = repository.fetchHumidity()  // + 1.5 сек
+                _weatherState.value = _weatherState.value.copy(humidity = humidity)
+                val windSpeed = repository.fetchWindSpeed()  // + 1 сек
+                _weatherState.value = _weatherState.value.copy(windSpeed = windSpeed)
+                _weatherState.value = _weatherState.value.copy(isLoading = false)
+
+            }
+            catch (e: Exception) {
+                _weatherState.value = _weatherState.value.copy(
+                    isLoading = false,
+                    error = "Ошибка загрузки: ${e.message}"
+                )
+        }
+    }
+}
+
+
+}
